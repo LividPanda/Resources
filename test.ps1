f5a2a1ed8f1548843d9136d2922ce504b6af79bf
LividPanda <33047160+LividPanda@users.noreply.github.com>
2020-07-31 20:07:18 -0500

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,1 @@
+([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll') }).GetType('Microsoft.Win32.UnsafeNativeMethods')
